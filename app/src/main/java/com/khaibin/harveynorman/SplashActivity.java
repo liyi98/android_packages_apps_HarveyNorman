4312c9322b39c72dfa50d7f7756a9a46dcfb82eb
khaibin <bkchow@yes.my>
2016-11-03 11:36:12 +0800

    Enable splash screen when launch

--- app/src/main/java/com/khaibin/harveynorman/SplashActivity.java
+++ app/src/main/java/com/khaibin/harveynorman/SplashActivity.java
@@ -12,7 +12,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = new Intent(this, MainActivity.class);
-        //startActivity(intent);
+        startActivity(intent);
         finish();
     }
 }
