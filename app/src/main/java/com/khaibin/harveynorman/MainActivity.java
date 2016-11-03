b7c8df02a657ef0b79dcc7bd9eb3663466fdae0a
khaibin <bkchow@yes.my>
2016-11-03 20:27:15 +0800

    Show loading spinning until the page is rendered

--- app/src/main/java/com/khaibin/harveynorman/MainActivity.java
+++ app/src/main/java/com/khaibin/harveynorman/MainActivity.java
@@ -1,8 +1,9 @@
 package com.khaibin.harveynorman;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
@@ -15,10 +16,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_webview);
-        WebView browser = (WebView) findViewById(R.id.webview);
-        browser.setWebViewClient(new WebViewClient());
+        final WebView browser = (WebView) findViewById(R.id.webview);
+        final ProgressBar progressloader = (ProgressBar) findViewById(R.id.progress_loader);
+        browser.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                if (progressloader.getVisibility() == View.VISIBLE && browser.getVisibility() == View.GONE) {
+                    progressloader.setVisibility(View.GONE);
+                    browser.setVisibility(View.VISIBLE);
+                }
+            }
+        });
         browser.getSettings().setJavaScriptEnabled(true);
         browser.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        browser.setHorizontalScrollBarEnabled(false);
         browser.loadUrl("http://harveynormanweb.github.io/");
     }
 
@@ -26,15 +37,14 @@ public class MainActivity extends AppCompatActivity {
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         WebView browser = (WebView) findViewById(R.id.webview);
 
-        if(event.getAction() == KeyEvent.ACTION_DOWN){
-            switch(keyCode)
-            {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
                 case KeyEvent.KEYCODE_BACK:
-                    if(browser.canGoBack()){
+                    if (browser.canGoBack()) {
                         canExit = false;
                         browser.goBack();
-                    }else{
-                        if(canExit) {
+                    } else {
+                        if (canExit) {
                             finish();
                         } else {
                             Toast.makeText(getApplicationContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
